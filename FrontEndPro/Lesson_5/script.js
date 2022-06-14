385314ac663542ea9abc95fd9296a7674a9cb444
Flifer <102440778+Flifer@users.noreply.github.com>
2022-06-14 21:48:40 +0200

    students marks

---
+++ FrontEndPro/Lesson_5/script.js
@@ -0,0 +1,59 @@
+const students = [
+    {
+      id: 10,
+      name: 'John Smith',
+      marks: [10, 8, 6, 9, 8, 7]
+    },
+    {
+      id: 11,
+      name: 'John Doe',
+      marks: [ 9, 8, 7, 6, 7]
+    },
+    {
+      id: 12,
+      name: 'Thomas Anderson',
+      marks: [6, 7, 10, 8]
+    },
+    {
+      id: 13,
+      name: 'Jean-Baptiste Emanuel Zorg',
+      marks: [10, 9, 8, 9]
+    }
+  ]
+
+averageStudentMark(Number(prompt('Choose student`s id')));
+
+averageGroupMark(students);
+
+function averageStudentMark(x) {
+
+    let student = students.find(student => student.id === x)
+
+        const markSum = student.marks.reduce((acc, curent) => {
+            const Sum = acc + curent;
+            return Sum });
+
+    let averageMark = markSum / student.marks.length
+
+return alert(`average mark of the student ${student.name} is ${averageMark}`)
+}
+
+function averageGroupMark(x) {
+
+    let averageMarksArray = []
+
+    for (let i = 0; i < x.length; i++) {
+        averageMarksArray.push(
+            x[i].marks.reduce((acc, curent) => {
+                const Sum = acc + curent;
+                return Sum }
+            ) / x[i].marks.length)
+    }
+
+    GroupMark=averageMarksArray.reduce((acc, curent) => {
+        const Sum = acc + curent;
+        return Sum }
+    ) / averageMarksArray.length
+
+return alert(`average mark of the group is ${GroupMark}`)
+}
