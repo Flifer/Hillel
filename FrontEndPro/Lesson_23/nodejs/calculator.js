b00b2f50147cf3c901d1f2d41460c1b9046f656b
Flifer <102440778+Flifer@users.noreply.github.com>
2022-08-21 16:51:34 +0200

    lesson 23 node.js

---
+++ FrontEndPro/Lesson_23/nodejs/calculator.js
@@ -0,0 +1,11 @@
+const add = require('./src/add');
+const sub = require('./src/sub');
+const mult = require('./src/mult');
+const div = require('./src/div');
+
+module.exports = {
+    add: add.add,
+    sub: sub.sub,
+    mult: mult.mult,
+    div: div.div,
+};
